6babdb494c559c880f3c715f110866c3da409bc5
FutureAxolotl <future_axotl@tutanota.com>
2022-08-26 13:43:57 -0400

    added easter egg route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :hellos
-  get "/hello", to: "hellos#index"
+  get "/hellos", to: "hellos#index"
   get "/articles", to: "articles#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
