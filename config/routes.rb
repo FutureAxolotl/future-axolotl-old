f66b8093eefa3698043d3a5644ed2d3c30be8b1e
FutureAxolotl <future_axotl@tutanota.com>
2022-08-27 09:06:43 -0400

    added link to posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   resources :posts
-  resources :hellos
   get "/articles", to: "articles#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
